ac3450efbf314852bf3bb7ba447ab60b2edf48e1
James Stroud <jstroud@mbi.ucla.edu>
2014-05-08 02:19:55 -0600

    Now ending POS at 310000

--- EBT.pro
+++ EBT.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 TARGET = ebt-qt
-VERSION = 3.0.0.1
+VERSION = 3.0.0.2
 INCLUDEPATH += src src/json src/qt
 macx:INCLUDEPATH += /usr/local/BerkeleyDB.4.8/include # /usr/local/include
 windows:INCLUDEPATH += C:/MinGW/msys/1.0/local/include/boost-1_55/
--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,6 +31,7 @@ namespace Checkpoints
         (  210001, uint256("0x000000057f97806270256152227d4a6995f0769907386ff2bfdd497bde0f5f53"))
         (  264656, uint256("0x0000000219e8f2ab079ce66d6cc68f7ede0c70c705c71c3d0896184f5e0c53b1"))
         (  281394, uint256("0xcaa48cfd4dc1eebe726d028efd989b9f348359e30ce07021132588679e544e5e"))
+        (  289000, uint256("0x00000000508a8d2fecdfcac6a05f14da0fda491a66a0d6d016db1fa9fe12c456"))
         ;
 
     static MapCheckpoints mapCheckpointsTestnet =
@@ -69,8 +70,8 @@ namespace Checkpoints
     }
 
     // ppcoin: synchronized checkpoint (centrally broadcasted)
-    uint256 hashSyncCheckpoint = uint256("0xcaa48cfd4dc1eebe726d028efd989b9f348359e30ce07021132588679e544e5e");
-    uint256 hashPendingCheckpoint = uint256("0xcaa48cfd4dc1eebe726d028efd989b9f348359e30ce07021132588679e544e5e");
+    uint256 hashSyncCheckpoint = uint256("0x00000000508a8d2fecdfcac6a05f14da0fda491a66a0d6d016db1fa9fe12c456");
+    uint256 hashPendingCheckpoint = uint256("0x00000000508a8d2fecdfcac6a05f14da0fda491a66a0d6d016db1fa9fe12c456");
     CSyncCheckpoint checkpointMessage;
     CSyncCheckpoint checkpointMessagePending;
     uint256 hashInvalidCheckpoint = 0;
